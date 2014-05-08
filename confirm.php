679c2e110c3cf04dc077847618754c7e38430776
fr-itaya <itaya@fact-real.com>
2014-05-08 07:13:46 +0000

    #23 確認画面：趣味欄に表示するよう修正

--- confirm.php
+++ confirm.php
@@ -1,3 +1,31 @@
+<?php
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$sex=$_POST['sex'];
+$postalcode=$_POST['postalcode'];
+$postalcode_view = implode('-', $postalcode);
+#3桁-4桁に区切ったものをハイフン入れて連結させたい←完了
+$prefecture=$_POST['prefecture'];
+$email=$_POST['email'];
+$comment=$_POST['comment'];
+
+if(isset($_POST["hobby"])){
+    $hobby = implode(' ', $_POST["hobby"]);
+}else{
+    $hobby = 'なし';
+}
+
+
+$first_name      = htmlspecialchars($first_name);
+$last_name       = htmlspecialchars($last_name);
+$sex             = htmlspecialchars($sex);
+$postalcode_view = htmlspecialchars($postalcode_view);
+$prefecture      = htmlspecialchars($prefecture);
+$email           = htmlspecialchars($email);
+$comment         = htmlspecialchars($comment);
+$hobby           = htmlspecialchars($hobby);
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,28 +35,6 @@
 </head>
 
 <body>
-<?php
-$first_name=$_POST['first_name'];
-$last_name=$_POST['last_name'];
-$sex=$_POST['sex'];
-$postalcode=$_POST['postalcode'];
-$postalcode_view = implode('-', $postalcode);
-#3桁-4桁に区切ったものをハイフン入れて連結させたい←完了
-$prefecture=$_POST['prefecture'];
-print $prefecture;#確認のため
-$email=$_POST['email'];
-$hobby=$_POST['hobby'];
-$comment=$_POST['comment'];
-
-$first_name=htmlspecialchars($first_name);
-$last_name=htmlspecialchars($last_name);
-$sex=htmlspecialchars($sex);
-$postalcode_view=htmlspecialchars($postalcode_view);
-$prefecture_view=htmlspecialchars($prefecture_view);
-$email=htmlspecialchars($email);
-$comment=htmlspecialchars($comment);
-?>
-
   <header>
     <h1>フォーム>確認</h1>
   </header>

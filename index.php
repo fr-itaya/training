303b05689360865eda61cb47e51480e484f001ae
fr-itaya <itaya@fact-real.com>
2014-05-08 00:49:21 +0000

    [WIP] #23 PHP化したファイルをcommit(終了画面以外)

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>issue #12</title>
+  <meta charset="utf-8">
+  <link href="style.css" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+<?php
+    print'<header>';
+    print'  <h1>フォーム>TOPページ</h1>';
+    print'</header>';
+    print'<nav>';
+    print'  <a href="form.php">フォームを入力する</a>';
+    print'</nav>';
+    print'<footer>';
+    print'<p>Copyright 2014</p>';
+    print'</footer>';
+?>
+</body>
+</html>
+

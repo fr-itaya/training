6401b7b69037ecbfcc9ed9533babcd74b3cf141d
fr-itaya <itaya@fact-real.com>
2014-05-26 08:12:27 +0000

    #48 jQueryによるDOM操作

--- JSjQuery/script.js
+++ JSjQuery/script.js
@@ -1,6 +1,13 @@
 //#47 課題3
-
+/*
 (function(msg) {
   var msg;
   alert(msg);
 })("Hello, World!");
+*/
+
+$(function() {
+  $('header').children().css('font-size', '30px');
+  $('#msg').append('<p class="sample">サンプル2です。</p>');
+  $('footer').text('Copyright 2013');
+});

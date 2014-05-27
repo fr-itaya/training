801ba8f5e1e093744e480046dbb8efa0a2c6d68f
fr-itaya <itaya@fact-real.com>
2014-05-27 09:05:22 +0000

    #49 jQueryホバーイベントによるDOM操作

--- JSjQuery/script.js
+++ JSjQuery/script.js
@@ -1,22 +1,25 @@
-//#47 課題3
-/*
-(function(msg) {
-  var msg;
-  alert(msg);
-})("Hello, World!");
-*/
-//#48 DOM操作
-/*
+//#49 hover event
 $(function() {
-  $('header').css('font-size', '30px');
+//$('header').css('font-size', '30px');
   $('#msg').append('<p class="sample">サンプル2です。</p>');
-  $('footer').text('Copyright 2013');
+//$('footer').text('Copyright 2013');
+$(function() {
+  $('.sample').hover(
+    function() {
+      $(this).css('background-color', '#ffa500'); 
+    },
+    function() {
+      $(this).css('background-color', '');
+    });
+  });
 });
-*/
+
 
 //#49 クリックイベント
+/*
 $(function() {
   $('#msg').click(function() {
     $(this).text('テストです。')
   });
 });
+*/

76f6404c5ac60c151baf3a10bb2d83f684f0f6d4
fr-itaya <itaya@fact-real.com>
2014-06-09 08:32:03 +0000

    #32 フォーム入力値からクエリ組立

--- done.php
+++ done.php
@@ -1,21 +1,37 @@
 <?php
 //★DB接続
 //PDOクラスのオブジェクトを作成
-$dsn = 'mysql:dbname=mysql_test;host=localhost';
+$dsn = 'mysql:dbname=mysql_test; host=localhost';
 $user = 'root';
 $password = '';
 //接続成功した時にオブジェクト作成
 try {
     $dbh = new PDO($dsn, $user, $password);
+//オブジェクト生成確認
+    if ($dbh == null) {
+        print('＼失敗！／<br />');
+    } else {
+        print('成功！<br />');
+    }
+//★データ追加
+//prepareメソッドでSQL文を作成
+    $sql = 'INSERT INTO users (last_name, first_name, email, pref_id, created_at) VALUES(:last_name, :first_name, :email, :pref_id, :created_at)';
+    $stmt = $dbh -> prepare($sql);
+//変数名にパラメータバインド
+    $stmt -> bindValue(':last_name',  $_POST['family_name'], PDO::PARAM_STR);
+    $stmt -> bindValue(':first_name', $_POST['given_name'],  PDO::PARAM_STR);
+    $stmt -> bindValue(':email',      $_POST['email'],       PDO::PARAM_STR);
+    $stmt -> bindValue(':pref_id',    $_POST['pref_id'],     PDO::PARAM_INT);
+    $stmt -> bindValue(':created_at', date('Y-m-d H:i:s'));
+//(この辺でSQLエスケープ処理？)
+//excecuteメソッドでSQL文をDBに発行
+    $stmt -> execute();
 } catch (PDOException $e) {
 //接続＼失敗！／した時の例外処理:エラー文言を表示
     print('Connection failed:'.$e -> getMessage());
+    var_dump(method_exists('PDO', 'dsn'));
     die(); 
 } 
-//★データ追加
-//prepareメソッドでSQL文を作成
-//(この辺でSQLエスケープ処理？)
-//excecuteメソッドでSQL文をDBに発行
 //★DBとの接続を切る
 $dbh = null;
 ?>

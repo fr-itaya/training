910ae414eb8bfbf1dabd51f40efee206abbd6d89
fr-itaya <itaya@fact-real.com>
2014-06-10 06:47:23 +0000

    #32 クエリ組立処理関数化・入力値をセッションで受渡・文字化け対策・コーディング規約に基づく修正

--- done.php
+++ done.php
@@ -1,34 +1,44 @@
 <?php
+//セッション変数使用
+session_start();
 //★DB接続
 //PDOクラスのオブジェクトを作成
-$dsn = 'mysql:dbname=mysql_test; host=localhost';
+$dsn = 'mysql:dbname=mysql_test; host=localhost; charset=utf8;';
 $user = 'root';
 $password = '';
-//接続成功した時にオブジェクト作成
-try {
-    $dbh = new PDO($dsn, $user, $password);
-//オブジェクト生成確認
-    if ($dbh == null) {
-        print('＼失敗！／<br />');
-    } else {
-        print('成功！<br />');
+//値の受取確認用(後で消します)
+var_dump($_SESSION);
+
+//★データ追加(関数化)
+function insertData($pdo) {
+    //prepareメソッドでSQL文を作成
+    $sql = 'INSERT INTO users (last_name, first_name, email, pref_id, created_at) 
+                   VALUES(:last_name, :first_name, :email, :pref_id, :created_at)';
+    $stmt = $pdo->prepare($sql);
+    //変数名にパラメータバインド
+    switch (true) { 
+        case !empty($_SESSION['family_name']): 
+            $stmt->bindValue(':last_name',  $_SESSION['family_name'], PDO::PARAM_STR);
+        case !empty($_SESSION['given_name']): 
+            $stmt->bindValue(':first_name', $_SESSION['given_name'],  PDO::PARAM_STR);
+        case !empty($_SESSION['email']): 
+            $stmt->bindValue(':email',      $_SESSION['email'],       PDO::PARAM_STR);
+        case !empty($_SESSION['pref_id']):
+            $stmt->bindValue(':pref_id',    $_SESSION['pref_id'],     PDO::PARAM_INT);
+            break; 
     }
-//★データ追加
-//prepareメソッドでSQL文を作成
-    $sql = 'INSERT INTO users (last_name, first_name, email, pref_id, created_at) VALUES(:last_name, :first_name, :email, :pref_id, :created_at)';
-    $stmt = $dbh -> prepare($sql);
-//変数名にパラメータバインド
-    $stmt -> bindValue(':last_name',  $_POST['family_name'], PDO::PARAM_STR);
-    $stmt -> bindValue(':first_name', $_POST['given_name'],  PDO::PARAM_STR);
-    $stmt -> bindValue(':email',      $_POST['email'],       PDO::PARAM_STR);
-    $stmt -> bindValue(':pref_id',    $_POST['pref_id'],     PDO::PARAM_INT);
-    $stmt -> bindValue(':created_at', date('Y-m-d H:i:s'));
-//(この辺でSQLエスケープ処理？)
-//excecuteメソッドでSQL文をDBに発行
-    $stmt -> execute();
+    $stmt->bindValue(':created_at', date('Y-m-d H:i:s'));
+    //excecuteメソッドでSQL文をDBに発行
+    $stmt->execute();
+}
+
+try {
+    //接続成功した時にオブジェクト作成
+    $dbh = new PDO($dsn, $user, $password);
+    insertData($dbh);
 } catch (PDOException $e) {
-//接続＼失敗！／した時の例外処理:エラー文言を表示
-    print('Connection failed:'.$e -> getMessage());
+    //接続＼失敗！／した時の例外処理:エラー文言を表示
+    print('Connection failed:'.$e->getMessage());
     var_dump(method_exists('PDO', 'dsn'));
     die(); 
 } 

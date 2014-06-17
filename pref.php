c39179479550e291e0cdfaf6ffb6e962e4caae33
fr-itaya <itaya@fact-real.com>
2014-06-17 08:56:55 +0000

    #33 コンストラクタに引数設定、セレクトボックス生成メソッド実装(DB接続部分除く)

--- pref.php
+++ pref.php
@@ -3,19 +3,34 @@ class Prefecture {
     public $pref_array;
     public $pref_id;
 
-    public function __construct() {
+    public function __construct($pref_selected) {
         $pref_array = array();
         //選択状態にするvalue値
-        if (empty($_SESSION['prefecture'])) {
+        if (empty($pref_selected)) {
             $pref_id = 0; //初期値は「--」
         }else{
-            $pref_id = $_SESSION['prefecture']; //選択した都道府県
+            $pref_id = $pref_selected; //選択した都道府県
         }
     }
 
     //都道府県セレクトボックス作成メソッド
     public function createSelectBoxTag($menu_tag) {
-    
+        //パラメータ値のチェック
+        if (!is_array($this->pref_array) || empty($this->pref_array)) {
+          return $menu_tag;
+        }
+        //selectタグ開始
+        $menu_tag .= '<select name="prefecture">';
+        //都道府県id-都道府県名の配列をoptionタグの値にする
+        foreach ($this->pref_array as $key => $value) {
+            $menu_tag .= '<option value="' . $key . '"';
+            //選択状態にするか調べる
+            if ($key==$this->pref_id) $menu_tag .= ' selected';
+                $menu_tag .= '>' . $value . '</option>';
+        }
+        //selectタグ終了
+        $menu_tag .= '</select>';
+        
     }
 
     //都道府県表示メソッド
@@ -24,27 +39,7 @@ class Prefecture {
     }
 }//end of class
 
-    //都道府県セレクトボックス作成
-    public function createSelectBoxTag() {
-
-        //パラメータ値のチェック
-        if (!is_array($prefecture_array) || empty($prefecture_array) || empty($prefecture_name)) {
-          return $menu_tag;
-        }
-
-        $menu_tag .= '<select name="prefecture">';
-        foreach ($prefecture_array as $key => $value) {
-            $menu_tag .= '<option value="' . $key . '"';
-            //選択状態にするか調べる
-            if ($key==$prefecture_sel_value) $menu_tag .= ' selected';
-                $menu_tag .= '>' . $value . '</option>';
-        }
-        $menu_tag .= '</select>';
-
-        return $menu_tag;
-    }
-
-    //都道府県リストをDBから取得
+    //都道府県リストをDBから取得(この処理も削除？何処でやるべき？)
     /*
     $stmt = $pdo->query('SELECT * FROM prefectures');
     $pref_none = array('--');
@@ -52,7 +47,7 @@ class Prefecture {
     $menu_array = array_merge($pref_none, $pref_rows);
      */
     
-    //セレクトボックス作成関数呼び出し
+    //セレクトボックス作成関数呼び出し(不要？)
     //$menu_tag = createSelectBoxTag($menu_array, $menu_name, $sel_value);
 
 

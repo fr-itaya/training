848a234dbfa91f78c9b93d96bde363d7c0d26ed6
fr-itaya <itaya@fact-real.com>
2014-06-25 08:04:24 +0000

    #34 インデントのズレ修正・ページナビ用外部ファイル用意

--- list.php
+++ list.php
@@ -2,21 +2,24 @@
 require_once('db_connect.php');
 require_once('db_fetch_pref.php');
 require_once('pref.php');
-
+require_once('page_nav.php');
 //ユーザ情報をDBから全取得
 function fetchUsers($pdo) {
     $stmt = $pdo->query('SELECT * FROM users');
     return $stmt;
 }
-
+//DBから都道府県リストを取得
 $pref_array = fetchPref($pdo);
 //create pref instance
 $pref = new Prefecture($pref_array);
+
+//取得したユーザ情報を1行ずつ配列に格納
 $user_list = array();
 $stmt = fetchUsers($pdo); 
 while ($user_row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    //都道府県IDを都道府県名に変換
     $pref->setPrefId($user_row['pref_id']);
-     $user_row['pref_id'] = $pref->getPrefById();
+    $user_row['pref_id'] = $pref->getPrefById();
     $user_list[] = $user_row;
 }
 /*
